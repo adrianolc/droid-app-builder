f0d7b519991b17fc9b74699cf37c364791a1104c
Adriano Cordeiro <adrianolcx@gmail.com>
2020-10-23 17:49:34 +0200

    Call data source from repo

--- data/src/main/java/adrianolc/appbuilder/data/repository/BranchRepositoryImpl.kt
+++ data/src/main/java/adrianolc/appbuilder/data/repository/BranchRepositoryImpl.kt
@@ -1,15 +1,21 @@
 package adrianolc.appbuilder.data.repository
 
+import adrianolc.appbuilder.data.mapper.toModel
+import adrianolc.appbuilder.data.remote.data_source.BranchDataSource
 import adrianolc.appbuilder.domain.model.Branch
 import adrianolc.appbuilder.domain.repository.BranchRepository
 
-class BranchRepositoryImpl : BranchRepository {
+class BranchRepositoryImpl(
+    private val dataSource: BranchDataSource
+) : BranchRepository {
 
     override suspend fun loadBranches(page: Int): List<Branch> {
-        return emptyList()
+        return dataSource.getBranches(page).map {
+            it.toModel()
+        }
     }
 
     override suspend fun getBranch(name: String): Branch {
-        TODO()
+        return dataSource.getBranch(name).toModel()
     }
 }
