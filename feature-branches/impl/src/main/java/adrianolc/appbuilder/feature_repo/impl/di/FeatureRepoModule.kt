bb3f6e32f7c5c12c742e7fedd75046a45f4d0154
Adriano Cordeiro <adrianolcx@gmail.com>
2022-05-23 18:59:37 +0200

    Fix build for new koin version

--- feature-branches/impl/src/main/java/adrianolc/appbuilder/feature_repo/impl/di/FeatureRepoModule.kt
+++ feature-branches/impl/src/main/java/adrianolc/appbuilder/feature_repo/impl/di/FeatureRepoModule.kt
@@ -4,7 +4,7 @@ import adrianolc.appbuilder.feature_repo.api.FeatureRepoNavigation
 import adrianolc.appbuilder.feature_repo.impl.FeatureRepoNavigationImpl
 import adrianolc.appbuilder.feature_repo.impl.domain.use_case.LoadBranchesUseCase
 import adrianolc.appbuilder.feature_repo.impl.ui.ListViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 private val viewModels = module {
