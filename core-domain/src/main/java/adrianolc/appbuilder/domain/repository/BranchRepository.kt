9a820e2be9d61df66882be33e930a53bea28330a
Adriano Cordeiro <adrianolcx@gmail.com>
2022-05-26 20:41:26 +0200

    Migrate to gradle kts

--- core-domain/src/main/java/adrianolc/appbuilder/domain/repository/BranchRepository.kt
+++ core-domain/src/main/java/adrianolc/appbuilder/domain/repository/BranchRepository.kt
@@ -3,8 +3,5 @@ package adrianolc.appbuilder.domain.repository
 import adrianolc.appbuilder.domain.model.Branch
 
 interface BranchRepository {
-
-    suspend fun loadBranches(page: Int): List<Branch>
-
     suspend fun getBranch(name: String): Branch
 }
