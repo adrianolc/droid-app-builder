258b658bdce0045afe1059ccb80ca572b3a54283
Adriano Cordeiro <adrianolcx@gmail.com>
2022-05-26 18:01:06 +0200

    Implement the plugins

--- build-logic/convention/src/main/java/appbuilder.android.feature.gradle.kts
+++ build-logic/convention/src/main/java/appbuilder.android.feature.gradle.kts
@@ -12,3 +12,26 @@ android {
          targetSdk = 32
     }
 }
+
+val libs: VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":remote:api"))
+
+    implementation(libs.findLibrary("koin.core").get())
+    implementation(libs.findLibrary("koin.android").get())
+
+    implementation(libs.findLibrary("androidx.core").get())
+    implementation(libs.findLibrary("androidx.appcompat").get())
+    implementation(libs.findLibrary("androidx.layout").get())
+    implementation(libs.findLibrary("androix.viewmodel").get())
+    implementation(libs.findLibrary("androidx.lifecycle").get())
+    implementation(libs.findLibrary("androidx.liveData").get())
+
+    implementation(libs.findLibrary("google.material").get())
+
+    testImplementation(libs.findLibrary("junit").get())
+    androidTestImplementation(libs.findLibrary("androidx.test.ext").get())
+    androidTestImplementation(libs.findLibrary("espresso").get())
+}
