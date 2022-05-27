d124f08eee4b9564b4f54632524c348557496274
Adriano Cordeiro <adrianolcx@gmail.com>
2022-05-27 15:10:49 +0200

    Add hilt plugin

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(libs.android.gradle.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.gradle.plugin)
+    }
+}
