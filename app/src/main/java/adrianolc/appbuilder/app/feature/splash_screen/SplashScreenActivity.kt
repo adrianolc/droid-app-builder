19bceb881c03f3fa06239749ced843e9cc08b3d3
Adriano Cordeiro <adrianolcx@gmail.com>
2022-05-27 17:42:02 +0200

    Open feature from splash

--- app/src/main/java/adrianolc/appbuilder/app/feature/splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/adrianolc/appbuilder/app/feature/splash_screen/SplashScreenActivity.kt
@@ -1,14 +1,19 @@
 package adrianolc.appbuilder.app.feature.splash_screen
 
+import adrianolc.appbuilder.feature_repo.api.FeatureRepoNavigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashScreenActivity : AppCompatActivity() {
 
-//    private val navigation: FeatureRepoNavigation by inject()
+    @Inject
+    lateinit var navigation: FeatureRepoNavigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +21,9 @@ class SplashScreenActivity : AppCompatActivity() {
         lifecycleScope.launch {
             delay(1000)
 
-//            startActivity(
-//                navigation.newIntent(this@SplashScreenActivity)
-//            )
+            startActivity(
+                navigation.newIntent(this@SplashScreenActivity)
+            )
         }
     }
 }
