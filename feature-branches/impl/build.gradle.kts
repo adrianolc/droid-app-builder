e24c3f729547ec8e77eabc79cc7fe929b8058d5f
Adriano Cordeiro <adrianolcx@gmail.com>
2022-06-14 20:52:28 +0200

    Add back api and impl for features (#38)
    * Merge api and impl modules from core-remote
    
    * Add back api and impl for features

---
+++ feature-branches/impl/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("appbuilder.android.feature")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    api(project(":feature-branches:api"))
+}
