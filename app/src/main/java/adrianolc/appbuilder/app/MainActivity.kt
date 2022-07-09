7d379897be1eb37cb49e922a4b187c279612388d
Adriano Cordeiro <adrianolcx@gmail.com>
2022-07-09 12:41:30 +0200

    Add main nav graph

--- app/src/main/java/adrianolc/appbuilder/app/MainActivity.kt
+++ app/src/main/java/adrianolc/appbuilder/app/MainActivity.kt
@@ -1,29 +1,15 @@
 package adrianolc.appbuilder.app
 
-import adrianolc.appbuilder.feature_repo.api.FeatureRepoNavigation
+import adrianolc.appbuilder.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var navigation: FeatureRepoNavigation
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        lifecycleScope.launch {
-            delay(1000)
-
-            startActivity(
-                navigation.newIntent(this@MainActivity)
-            )
-        }
+        setContentView(R.layout.main_activity)
     }
 }
